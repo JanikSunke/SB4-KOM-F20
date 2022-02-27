243d369ffd51107f6048328d26ba7ac2862b205a
JanikSunke <janik.sunke@hotmail.dk>
2022-02-27 21:57:22 +0100

    MINE

--- NetbeanJasun20/Enemy/src/main/java/dk/sdu/mmmi/cbse/enemysystem/EnemyControlSystem.java
+++ NetbeanJasun20/Enemy/src/main/java/dk/sdu/mmmi/cbse/enemysystem/EnemyControlSystem.java
@@ -18,59 +18,50 @@ public class EnemyControlSystem implements IEntityProcessingService {
 
     @Override
     public void process(GameData gameData, World world) {
-
         for (Entity enemy : world.getEntities(Enemy.class)) {
             PositionPart positionPart = enemy.getPart(PositionPart.class);
             MovingPart movingPart = enemy.getPart(MovingPart.class);
-            LifePart lifePart = enemy.getPart(LifePart.class);
-
-            Random rand = new Random();
-
-            float rng = rand.nextFloat();
-
-            if (rng > 0.1f && rng < 0.9f) {
-                movingPart.setUp(true);
-            }
-
-            if (rng < 0.2f) {
+            int move = (int)(Math.random() * 10);
+            if (move < 2) {
                 movingPart.setLeft(true);
-            }
-
-            if (rng > 0.8f) {
+                movingPart.setRight(false);
+            } else if (move > 9) {
                 movingPart.setRight(true);
+                movingPart.setLeft(false);
+            } else {
+                movingPart.setLeft(false);
+                movingPart.setRight(false);
+            }
+            if (move < 2) {
+                movingPart.setUp(true);
             }
 
             movingPart.process(gameData, enemy);
             positionPart.process(gameData, enemy);
-            lifePart.process(gameData, enemy);
 
             updateShape(enemy);
-
-            movingPart.setRight(false);
-            movingPart.setLeft(false);
-            movingPart.setUp(false);
         }
     }
 
     private void updateShape(Entity entity) {
-        float[] shapex = new float[4];
-        float[] shapey = new float[4];
+        float[] shapex = entity.getShapeX();
+        float[] shapey = entity.getShapeY();
         PositionPart positionPart = entity.getPart(PositionPart.class);
         float x = positionPart.getX();
         float y = positionPart.getY();
         float radians = positionPart.getRadians();
 
-        shapex[0] = (float) (x + Math.cos(radians) * entity.getRadius());
-        shapey[0] = (float) (y + Math.sin(radians) * entity.getRadius());
+        shapex[0] = (float) (x + Math.cos(radians) * 4);
+        shapey[0] = (float) (y + Math.sin(radians) * 4);
 
-        shapex[1] = (float) (x + Math.cos(radians - 4 * 3.1415f / 5) * entity.getRadius());
-        shapey[1] = (float) (y + Math.sin(radians - 4 * 3.1145f / 5) * entity.getRadius());
+        shapex[1] = (float) (x + Math.cos(radians - 4 * 3.1415f / 5) * 10);
+        shapey[1] = (float) (y + Math.sin(radians - 4 * 3.1145f / 5) * 10);
 
-        shapex[2] = (float) (x + Math.cos(radians + 3.1415f) * entity.getRadius() * 0.5);
-        shapey[2] = (float) (y + Math.sin(radians + 3.1415f) * entity.getRadius() * 0.5);
+        shapex[2] = (float) (x + Math.cos(radians + 3.1415f) * 12);
+        shapey[2] = (float) (y + Math.sin(radians + 3.1415f) * 12);
 
-        shapex[3] = (float) (x + Math.cos(radians + 4 * 3.1415f / 5) * entity.getRadius());
-        shapey[3] = (float) (y + Math.sin(radians + 4 * 3.1415f / 5) * entity.getRadius());
+        shapex[3] = (float) (x + Math.cos(radians + 4 * 3.1415f / 5) * 10);
+        shapey[3] = (float) (y + Math.sin(radians + 4 * 3.1415f / 5) * 10);
 
         entity.setShapeX(shapex);
         entity.setShapeY(shapey);
--- NetbeanJasun20/Enemy/src/main/java/dk/sdu/mmmi/cbse/enemysystem/EnemyPlugin.java
+++ NetbeanJasun20/Enemy/src/main/java/dk/sdu/mmmi/cbse/enemysystem/EnemyPlugin.java
@@ -29,24 +29,17 @@ public class EnemyPlugin implements IGamePluginService {
     private Entity createEnemyShip(GameData gameData) {
 
         float deacceleration = 10;
-        float acceleration = 150;
-        float maxSpeed = 200;
+        float acceleration = 100;
+        float maxSpeed = 150;
         float rotationSpeed = 5;
-        float x = new Random().nextFloat() * gameData.getDisplayWidth();
-        float y = new Random().nextFloat() * gameData.getDisplayHeight();
+        float x = 20;
+        float y = 20;
         float radians = 3.1415f / 2;
 
-        float[] colour = new float[4];
-        colour[0] = 1.0f;
-        colour[1] = 0.0f;
-        colour[2] = 0.0f;
-        colour[3] = 1.0f;
-
         Entity enemyShip = new Enemy();
-        enemyShip.setRadius(8);
         enemyShip.add(new MovingPart(deacceleration, acceleration, maxSpeed, rotationSpeed));
         enemyShip.add(new PositionPart(x, y, radians));
-        enemyShip.add(new LifePart(1));
+        enemyShip.setRadius(8);
 
         return enemyShip;
     }
